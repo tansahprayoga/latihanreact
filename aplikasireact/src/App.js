60e0ac7ee7e1165ebbc5da0f23374edcf014c5c6
tansah prayoga <prayogatansah@gmail.com>
2020-07-21 10:53:35 +0700

    Menggunakan React Router

--- aplikasireact/src/App.js
+++ aplikasireact/src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import Parent from './Component/Class/Parent';
-import BootstrapComp from './Component/Class/BootstrapComp';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import NavbarComp from './Component/Fungsional/NavbarComp';
+import HomePage from './Component/Fungsional/HomePage';
+import About from './Component/Fungsional/AboutComp';
 //import logo from './logo.svg';
 //import Home from './Component/Fungsional/Home';
 //import Beranda from './Component/Class/Beranda';
 
 //import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <Parent />
-      <BootstrapComp/>
+     
+      <BrowserRouter>
+      <NavbarComp />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={About} />
+      </Switch>
+
+    </BrowserRouter>
+   
 
     </div>
   );
