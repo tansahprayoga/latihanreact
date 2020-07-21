0c4fa5051e2aee37e101ede08838650b8b8a04aa
tansah prayoga <prayogatansah@gmail.com>
2020-07-21 09:58:56 +0700

    Menggunakan Bootstrap pada React.Js

--- aplikasireact/src/App.js
+++ aplikasireact/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Parent from './Component/Class/Parent';
+import BootstrapComp from './Component/Class/BootstrapComp';
 //import logo from './logo.svg';
 //import Home from './Component/Fungsional/Home';
 //import Beranda from './Component/Class/Beranda';
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <Parent />
+      <BootstrapComp/>
 
     </div>
   );
