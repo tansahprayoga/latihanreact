60b72618287b2dd5d3b805eab5b190ec170f9d42
tansah prayoga <prayogatansah@gmail.com>
2020-07-21 15:50:22 +0700

    Delete data Rest Api

--- aplikasireact/src/Component/Class/ListComp.jsx
+++ aplikasireact/src/Component/Class/ListComp.jsx
@@ -1,12 +1,10 @@
 import React, { PureComponent } from 'react';
 import axios from 'axios'
 import qs from 'querystring'
-
 import { Table, Button, Container, NavLink, Alert } from 'reactstrap'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-
-const api = 'http://localhost:3002'
+const api = 'http://localhost:3001'
 
 class ListComp extends PureComponent {
     constructor(props) {
@@ -17,21 +15,51 @@ class ListComp extends PureComponent {
             display: 'none'
         }
     }
-
     componentDidMount() {
-        
         axios.get(api + '/tampil').then(res => {
             this.setState({
                 mahasiswa: res.data.values
             })
         })
     }
+
+    Deletemahasiswa = (idmahasiswa) => {
+        const { mahasiswa } = this.state
+        const data = qs.stringify({
+            id_mahasiswa: idmahasiswa
+        })
+
+        axios.delete(api + '/hapus',
+            {
+                data: data,
+                headers: { 'Content-type': 'application/x-www-form-urlencoded' }
+            }
+        ).then(json => {
+            if (json.data.status === 200) {
+                this.setState({
+                    response: json.data.values,
+                    mahasiswa: mahasiswa.filter(mahasiswa => mahasiswa.id_mahasiswa !== idmahasiswa),
+                    dislpay: 'block'
+                })
+                //this.props.history.push('/mahasiswa')
+            } else {
+                this.setState({
+                    response: json.data.values,
+                    dislpay: 'block'
+                })
+                //this.props.history.push('/mahasiswa')
+            }
+        })
+    }
+
     render() {
         return (
             <Container>
                 <h2>Data mahasiswa</h2>
+                <Alert color="success" style={{ dislpay: this.state.display }}>
+                    {this.state.response}
+                </Alert>
                 <NavLink href="/mahasiswa/tambah"><Button color="success">Tambah Data</Button></NavLink>
-               
                 <hr />
                 <Table className="table-bordered">
                     <thead>
@@ -48,7 +76,6 @@ class ListComp extends PureComponent {
                                 <td>{mahasiswa.nim}</td>
                                 <td>{mahasiswa.nama}</td>
                                 <td>{mahasiswa.jurusan}</td>
-                                
                                 <td>
                                     <Link to=
                                         {
@@ -64,11 +91,11 @@ class ListComp extends PureComponent {
                                         }>
                                         <Button>Edit</Button>
                                     </Link>
+                                    <span> </span>
+                                    <Button onClick={() => this.Deletemahasiswa(mahasiswa.id_mahasiswa)} color="danger">Hapus</Button>
                                 </td>
                             </tr>
-
-                            )}
-                        
+                        )}
                     </tbody>
                 </Table>
             </Container>
